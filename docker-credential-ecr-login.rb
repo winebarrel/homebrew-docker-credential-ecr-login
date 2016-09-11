2e3c5add525bb44182e2efef4476432846a4828f
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2016-09-11 19:46:32 +0900

    Update docker-credential-ecr-login.rb
    small fix

--- docker-credential-ecr-login.rb
+++ docker-credential-ecr-login.rb
@@ -5,7 +5,7 @@ class DockerCredentialEcrLogin < Formula
   VERSION = '1614bf09f10aaa1995647a631ba0f28536d8e417'
 
   homepage 'https://github.com/awslabs/amazon-ecr-credential-helper'
-  url "https://github.com/awslabs/amazon-ecr-credential-helper/archive/1614bf09f10aaa1995647a631ba0f28536d8e417.tar.gz"
+  url "https://github.com/awslabs/amazon-ecr-credential-helper/archive/#{VERSION}.tar.gz"
   sha256 'b7e01deb04d11a561d88a70e2c47f4aa83901daba77b5d1973744f4efc98eda5'
   version VERSION
   head 'https://github.com/awslabs/amazon-ecr-credential-helper.git', :branch => 'master'
